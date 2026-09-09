111a4f70bdfc52cb4fdd49bf393fed3260e3a6d2
kinderDmitry <kinderMikhail0vich@gmail.com>
2026-09-09 20:52:50 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.marketforecast.prox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 115
-        versionName = "4.8.74"
+        versionCode = 116
+        versionName = "4.8.75"
     }
     buildFeatures { compose = true }
 
